7fcbc7d48cf32920a8b2561b1aaebcc17bd4b428
suvroneel2001@gmail.com <suvroneel2001@gmail.com>
2025-01-16 23:13:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mydemoapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.mydemoapplication;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("Aggin", "This is a debug message");
+        Log.d("Aggin","DEMO TEST");
+
+    }
+}
